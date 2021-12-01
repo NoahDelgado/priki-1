73ccb4bed13c789c1f5f8b0f41f05261df4a8b79
XCarrel <Xavier.Carrel@cpnv.ch>
2021-12-01 20:49:48 +0100

    Select the practices using state and modification date

--- app/Models/Practice.php
+++ app/Models/Practice.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,23 @@ class Practice extends Model
     public function domain()
     {
         return $this->belongsTo(Domain::class);
+    }
+
+    public function publicationState()
+    {
+        return $this->belongsTo(PublicationState::class);
+    }
+
+    /**
+     * Returns all practicies that have been modified within the last nbDays and that are in published state
+     * @param $nbDays
+     * @return mixed
+     */
+    static function publishedModifiedOnes($nbDays)
+    {
+        return self::where('updated_at', '>=', Carbon::now()->subDays($nbDays))
+            ->whereHas('publicationState', function ($q) {
+                $q->where('slug','PUB');
+            })->get();
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h2></h2>
     <p>Pratiques mises à jour dans les <input id="numDays" class="col-1 text-center" type="number" value="{{ $filtervalue }}"> derniers jours</p>
-    @foreach(\App\Models\Practice::all() as $practice)
+    @foreach(\App\Models\Practice::publishedModifiedOnes($filtervalue) as $practice)
         <div class="card p-2 m-2">{{ $practice->description }}<div class="card-footer small">Domaine: {{ $practice->domain->name }}, mis à jour: {{ Carbon\Carbon::make($practice->updated_at)->isoformat('D MMMM Y') }}</div></div>
     @endforeach
 @endsection
