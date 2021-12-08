3605d3c6e27ea74956788c8f44d039bb97b43b4d
XCarrel <Xavier.Carrel@cpnv.ch>
2021-12-08 11:41:44 +0100

    Translate login form in french
    Using locale

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // Messages
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    // Labels
+    'Password' => 'Mot de passe',
+    'Email' => 'Adresse email',
+    'Remember me' => 'Se souvenir de moi',
+    'Forgot your password?' => 'Mot de passe oublié ?',
+    'Log in' => 'Connexion',
+
+];
