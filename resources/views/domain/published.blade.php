ae5d43eea3fcb0da113b7c893962e1a9ae648aac
XCarrel <Xavier.Carrel@cpnv.ch>
2021-12-09 16:33:05 +0100

    Re-use practice component in domain view

--- resources/views/domain/published.blade.php
+++ resources/views/domain/published.blade.php
@@ -1,13 +1,7 @@
 @extends ('layout')
 
 @section ('content')
-    <h2>Pratiques publiées</h2>
     @foreach($practices as $practice)
-        <div class="card p-2 m-2">
-            {{ $practice->description }}
-            <div class="card-footer small">
-                Mis à jour: {{ Carbon\Carbon::make($practice->updated_at)->isoformat('D MMMM Y') }}
-            </div>
-        </div>
+        <livewire:practice :practice="$practice"/>
     @endforeach
 @endsection
